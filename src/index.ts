99fb495778cb588fa3ce0969cfd8608ae17b068c
chanmako <mgp7ry@gmail.com>
2021-01-27 19:19:05 +0900

    add generics

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,13 @@
 // const sum = sumProductsPrice(100, 200, 300, 400, 500)
 // console.log('Function Param sample 5:', sum)
 
-import objectSample from './object/object'
-import typeAliasSample from './object/alias'
-objectSample()
-typeAliasSample()
+// import objectSample from './object/object'
+// import typeAliasSample from './object/alias'
+// objectSample()
+// typeAliasSample()
+
+import genericsBasicSample from './generics/basic'
+genericsBasicSample()
+
+import genericsAdvSample from './generics/adv'
+genericsAdvSample()
